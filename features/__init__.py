b6c77b5c9d0d9fbc07ac88d502d6338ffe3ff611
makrand999 <143881177+makrand999@users.noreply.github.com>
2025-05-07 01:00:21 +0530

    Update __init__.py

--- features/__init__.py
+++ features/__init__.py
@@ -5,5 +5,6 @@ This makes the features directory a proper Python package.
 
 # Import feature functions here to make them available at the package level
 from .image_compressor import compress_image
+from . img_to_pdf import images_to_pdf
 
-__all__ = ['compress_image']
+__all__ = ['compress_image' , 'images_to_pdf']
